013993075a30f34ea5a67461f966857f8ab06956
francis-mwas <mwangif184@gmail.com>
2022-06-08 23:16:35 +0300

    tesed creation of other roles application roles

--- ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/user/RoleRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/user/RoleRepositoryTests.java
@@ -7,21 +7,33 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
 public class RoleRepositoryTests {
 
     @Autowired
     private RoleRepository roleRepository;
 
     @Test
-    @Rollback(value = false)
     public void testCreateFirstRole(){
         Role roleAdmin =new Role("Admin", "manage everything");
         Role savedRole = roleRepository.save(roleAdmin);
        assertThat(savedRole.getId()).isGreaterThan(0);
+
+    }
+    @Test
+    public void testCreateRestRoles(){
+        Role roleSalesperson =new Role("Salesperson", "manage product price, " +"customers, shipping, orders and sales report");
+        Role roleEditor =new Role("Editor", "manage categories, brands, " +"products, articles and menus");
+        Role roleShipper = new Role("Shipper", "view products, view orders and update order status");
+        Role roleAssistant = new Role("Assistant", "manage questions and reviews");
+
+        roleRepository.saveAll(List.of(roleSalesperson, roleEditor, roleShipper, roleAssistant));
 
     }
 
