a99bba4c7a7161b0892f6189e387e853861076fd
francis-mwas <mwangif184@gmail.com>
2022-06-08 22:21:47 +0300

    First commit of building ecommerce application with spring boot

---
+++ ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/user/RoleRepositoryTests.java
@@ -0,0 +1,29 @@
+package com.shopme.admin.user;
+
+import com.shopme.common.entity.Role;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class RoleRepositoryTests {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Test
+    @Rollback(value = false)
+    public void testCreateFirstRole(){
+        Role roleAdmin =new Role("Admin", "manage everything");
+        Role savedRole = roleRepository.save(roleAdmin);
+       assertThat(savedRole.getId()).isGreaterThan(0);
+
+    }
+
+}
+
